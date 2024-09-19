dcc603d9c3b002da1de1339afc2fd8c593cf2da8
shortytheman <shor7ty@gmail.com>
2024-09-19 22:34:34 +0200

    Button/footer

--- client/smartmad/src/components/button.js
+++ client/smartmad/src/components/button.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const Button = () => {
-  return <button>Click me</button>;
+  const [hover, setHover] = React.useState(false);
+  return (
+    <button
+      style={{
+        backgroundColor: hover ? '#87CEEB' : '#ADD8E6', // light blue
+        border: '1px solid #03055B', // dark blue
+        borderRadius: '5px', // rounded edges
+        padding: '10px 20px',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        color: '#000', // black text
+        cursor: 'pointer',
+      }}
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+    >
+      Opret
+    </button>
+  );
 };
 
 export default Button;
---
+++ client/smartmad/src/components/footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: '#333',
+        color: '#fff',
+        padding: '20px',
+        textAlign: 'center',
+        position: 'fixed',
+        bottom: 0,
+        width: '100%',
+      }}
+    >
+      <p>&copy; SmartMad</p>
+      <p>
+        <a href="www.smartmad.dk/blabla" style={{ color: '#fff', textDecoration: 'none' }}>
+          blabla
+        </a>
+        {' | '}
+        <a href="www.smartmad.dk/blabla1" style={{ color: '#fff', textDecoration: 'none' }}>
+          blabla1
+        </a>
+      </p>
+      <p>
+        <a href="www.smartmad.dk/socials" style={{ color: '#fff', textDecoration: 'none' }}>
+          Follow us on social media:
+        </a>
+        {' '}
+        <a href="www.smartmad.dk/facebook" style={{ color: '#fff', textDecoration: 'none' }}>
+          <i className="fa fa-facebook" />
+        </a>
+        {' '}
+        <a href="www.smartmad.dk/instagram" style={{ color: '#fff', textDecoration: 'none' }}>
+          <i className="fa fa-instagram" />
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
