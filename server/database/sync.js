2f81543266fc58718552c35a52b902318889d67d
shortytheman <shor7ty@gmail.com>
2024-11-17 12:22:23 +0100

    serverREADME creation and schema.sql optimized with user creation

--- server/database/sync.js
+++ server/database/sync.js
@@ -1,13 +1,5 @@
 // node sync.js
 
-/* 
-Manually create user with user_id 1:
-
-INSERT INTO users (username, password, createdAt, updatedAt) 
-VALUES ('user_12345', 'password_67890', NOW(), NOW());
-
-*/
-
 import sequelize from './database.js';
 import User from '../models/user.js';
 import Recipe from '../models/recipe.js';
