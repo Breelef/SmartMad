5aa8a906342010a21b65b70cbbbea67d3dceb30f
Breelef <92298494+Breelef@users.noreply.github.com>
2025-02-21 10:22:58 +0100

    redis

--- server/auth/redisOperations.js
+++ server/auth/redisOperations.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config({path: "../"})
 export const redis = new Redis({
-  port: process.env.REDIS_PORT,
-  host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST,
+    password: process.env.REDIS_PASSWORD,
 })
 
 /**
